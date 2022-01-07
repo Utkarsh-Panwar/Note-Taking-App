d243aa76b2e52b599437c2310d122b51ea95bda7
Utkarsh Panwar <utkarshpanwar72@gmail.com>
2022-01-07 15:28:48 +0530

    Note-Taking-App

---
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), INotesRVAdapter {
+
+    lateinit var viewModel: NoteViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView.layoutManager=LinearLayoutManager(this)
+        val adapter=NotesRVAdapter(this,this)
+        recyclerView.adapter=adapter
+
+        viewModel=ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(application))
+            .get(NoteViewModel::class.java)
+
+        viewModel.allNotes.observe(this, Observer {list->
+            list.let{
+                adapter.updateList(it)
+            }
+        })
+    }
+
+    override fun onItemClicked(note: Note) {
+        viewModel.deleteNote(note)
+        Toast.makeText(this,"${note.text} Deleted",Toast.LENGTH_LONG).show()
+    }
+
+    fun submitData(view: android.view.View) {
+
+        val notetext=input.text.toString()
+        if(notetext.isNotEmpty()){
+
+            viewModel.insertNote(Note(notetext))
+            Toast.makeText(this,"$notetext Inserted",Toast.LENGTH_LONG).show()
+
+        }
+    }
+}
